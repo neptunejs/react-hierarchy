9c32f01f7673e432fe208a73595cbede3b7e8fb7
Daniel Kostro <kostro.d@gmail.com>
2017-04-04 14:17:42 +0200

    TimeTreeD3: transitions

--- src/components/TimeTreeD3.js
+++ src/components/TimeTreeD3.js
@@ -2,104 +2,148 @@ import React, {Component} from 'react';
 import {TimeAxis} from 'react-axis';
 import {scaleTime, scaleLinear} from 'd3-scale';
 import {select} from 'd3-selection';
+import 'd3-transition';
 import flexTree from 'd3-flextree-v4';
+
+const UPDATE_TRANSITION_DURATION = 1000;
+const ENTER_EXIT_TRANSITION_DURATION = 500;
 
 class TimeTreeD3 extends Component {
     d3Render() {
-        const svg = this.refs.svgTree;
 
-        const leaf = this.leaf;
-        const root = this.root;
-        const beginTime = root.data.time;
-        const endTime = leaf.data.time;
 
-        const scaleX = scaleLinear()
-            .domain([this.minx, this.maxx])
-            .range([0, 1000]);
-
-        const scale = scaleTime()
-            .domain([beginTime, endTime])
-            .range([0, 1000]);
-
-        root.each((node) => {
-            node.y = scaleX(node.x);//node.x;
-            node.x = scale(node.data.time);
-        });
-
-        const nodes = [];
-        const links = [];
-        root.each(node => {
-            nodes.push(node);
-            if (node.parent) {
-                links.push(node);
+        const update = (rootNode, isChild) => {
+            var updateTransitionWait, enterExitTransitionWait;
+            if (isChild) {
+                updateTransitionWait = ENTER_EXIT_TRANSITION_DURATION;
+                enterExitTransitionWait = 0;
+            } else {
+                updateTransitionWait = 0;
+                enterExitTransitionWait = UPDATE_TRANSITION_DURATION;
             }
-        });
+            this.init(rootNode);
 
-        function update() {
+            const leaf = this.leaf;
+            const root = this.root;
+            const beginTime = root.data.time;
+            const endTime = leaf.data.time;
+
+            const scaleX = scaleLinear()
+                .domain([this.minx, this.maxx])
+                .range([0, 1000]);
+
+            const scale = scaleTime()
+                .domain([beginTime, endTime])
+                .range([0, 1000]);
+
+            root.each((node) => {
+                // node.y = scaleX(node.x);//node.x;
+                node.realY = scaleX(node.x);
+                node.realX = scale(node.data.time);
+                // node.x = scale(node.data.time);
+            });
+
+            const svg = this.refs.svgTree;
+
+
+            const nodes = [];
+            const links = [];
+            rootNode.each(node => {
+                nodes.push(node);
+                if (node !== rootNode && node.parent) {
+                    links.push(node);
+                }
+            });
+
             const updateCircle = el => {
                 return el
-                    .attr('cx', node => node.x)
-                    .attr('cy', node => node.y)
-                    .attr('r', 3);
+                    .attr('cx', node => node.realX)
+                    .attr('cy', node => node.realY)
+                    .attr('r', 5);
             };
 
             const updateLine1 = el => {
                 return el
-                    .attr('x1', node => node.parent.x)
-                    .attr('y1', node => node.parent.y)
-                    .attr('x2', node => node.parent.x)
-                    .attr('y2', node => node.y);
+                    .attr('x1', node => node.parent.realX)
+                    .attr('y1', node => node.parent.realY)
+                    .attr('x2', node => node.parent.realX)
+                    .attr('y2', node => node.realY);
             };
 
             const updateLine2 = el => {
                 return el
-                    .attr('x1', node => node.parent.x)
-                    .attr('y1', node => node.y)
-                    .attr('x2', node => node.x)
-                    .attr('y2', node => node.y);
+                    .attr('x1', node => node.parent.realX)
+                    .attr('y1', node => node.realY)
+                    .attr('x2', node => node.realX)
+                    .attr('y2', node => node.realY);
             };
-
-            const gCircle = select(svg)
-                .selectAll('g.node')
-                .data(nodes, node => node.data.name);
-
-            // Update
-            updateCircle(gCircle.select('circle'));
-
-
-            // New
-            const circles = gCircle.enter()
-                .append('g')
-                .attr('class', 'node')
-                .append('circle');
-
-            updateCircle(circles);
-
-            // Remove
-            gCircle.exit().remove();
 
             const d3Links = select(svg)
                 .selectAll('g.link')
                 .data(links, link => link.data.name);
 
             // Update
-            updateLine1(d3Links.select('line:nth-child(1)'));
-            updateLine2(d3Links.select('line:nth-child(2)'));
+            updateLine1(d3Links.select('line:nth-child(1)').transition().duration(updateTransitionWait).transition().duration(UPDATE_TRANSITION_DURATION));
+            updateLine2(d3Links.select('line:nth-child(2)').transition().duration(updateTransitionWait).transition().duration(UPDATE_TRANSITION_DURATION));
 
             // New
             const gLink = d3Links.enter()
                 .append('g')
                 .attr('class', 'link')
-                .attr('stroke', 'black');
+                .attr('stroke', 'black')
+                .attr('stroke-opacity', 0);
+
+            gLink.transition().duration(enterExitTransitionWait)
+                .transition().duration(ENTER_EXIT_TRANSITION_DURATION)
+                .attr('stroke-opacity', 1);
 
             updateLine1(gLink.append('line'));
             updateLine2(gLink.append('line'));
 
             // Remove
-            d3Links.exit().remove();
-        }
+            d3Links.exit()
+                .transition().duration(enterExitTransitionWait)
+                .transition().duration(ENTER_EXIT_TRANSITION_DURATION)
+                .attr('stroke-opacity', 0)
+                .remove();
 
-        update(root);
+
+            const gCircle = select(svg)
+                .selectAll('g.node')
+                .data(nodes, node => node.data.name);
+
+            // Update node
+            updateCircle(gCircle.select('circle').transition().duration(updateTransitionWait).transition().duration(UPDATE_TRANSITION_DURATION));
+
+
+            // New node
+            let circles = gCircle.enter()
+                .append('g')
+                .attr('class', 'node')
+                .attr('fill-opacity', 0);
+
+
+            circles.transition().duration(enterExitTransitionWait).transition().duration(ENTER_EXIT_TRANSITION_DURATION)
+                .attr('fill-opacity', 1);
+
+
+            circles = circles.append('circle')
+                .on('click', d => {
+                    update(d, true);
+                });
+
+
+            updateCircle(circles);
+
+
+            // Remove
+            gCircle.exit()
+                .transition().duration(enterExitTransitionWait)
+                .transition().duration(ENTER_EXIT_TRANSITION_DURATION)
+                .attr('fill-opacity', 0).remove();
+        };
+
+        update(this.root);
     }
 
     componentDidMount() {
@@ -173,7 +217,7 @@ class TimeTreeD3 extends Component {
                 <svg x="0" y="1050" width="1000" height="50" viewBox="0 0 1000 50">
                     <TimeAxis position="bottom" beginTime={beginTime}
                               endTime={endTime}
-                              width={1000} height={50} />
+                              width={1000} height={50}/>
                 </svg>
             </svg>
         );
