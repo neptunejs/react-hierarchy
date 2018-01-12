d4121d5519aea7cb0c659eb1c33a91ee6f0cc070
Daniel Kostro <kostro.d@gmail.com>
2018-01-12 10:13:49 +0100

    Updates

---
+++ TimeTree/PieChart.js
@@ -0,0 +1,117 @@
+// Adapted from https://github.com/brigade/react-simple-pie-chart
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Brigade
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * @param {object[]} slices
+ * @param {object} [options]
+ * @return {object[]}
+ */
+function renderPaths(slices, options) {
+  const total = slices.reduce((totalValue, { value }) => totalValue + value, 0);
+
+  let radSegment = 0;
+  let lastX = options.radius;
+  let lastY = 0;
+
+  return slices.map(({ color, value }, index) => {
+    // Should we just draw a circle?
+    if (value === total) {
+      return (
+        <circle
+          r={options.radius}
+          cx={options.center}
+          cy={options.center}
+          fill={color}
+          key={index}
+        />
+      );
+    }
+
+    if (value === 0) {
+      return null;
+    }
+
+    const valuePercentage = value / total;
+
+    // Should the arc go the long way round?
+    const longArc = (valuePercentage <= 0.5) ? 0 : 1;
+
+    radSegment += valuePercentage * options.radCircumference;
+    const nextX = Math.cos(radSegment) * options.radius;
+    const nextY = Math.sin(radSegment) * options.radius;
+
+    // d is a string that describes the path of the slice.
+    // The weirdly placed minus signs [eg, (-(lastY))] are due to the fact
+    // that our calculations are for a graph with positive Y values going up,
+    // but on the screen positive Y values go down.
+    const d = [
+      `M ${options.center},${options.center}`,
+      `l ${lastX},${-lastY}`,
+      `a${options.radius},${options.radius}`,
+      '0',
+      `${longArc},0`,
+      `${nextX - lastX},${-(nextY - lastY)}`,
+      'z',
+    ].join(' ');
+
+    lastX = nextX;
+    lastY = nextY;
+
+    return <path d={d} fill={color} key={index} />;
+  });
+}
+
+/**
+ * Generates an SVG pie chart.
+ * @see {http://wiki.scribus.net/canvas/Making_a_Pie_Chart}
+ */
+export default function PieChart(props) {
+  const size = props.size;
+  const options = {
+    radCircumference: Math.PI * 2,
+    center: Math.floor(size / 2),
+    radius: Math.floor(size / 2) - 1
+  };
+  return (
+    <g transform={`translate(${-options.center},${-options.center}) rotate(-90 ${options.center} ${options.center})`}>
+      {renderPaths(props.slices, options)}
+    </g>
+  );
+}
+
+PieChart.defaultProps = {
+  size: 20
+};
+
+PieChart.propTypes = {
+  slices: PropTypes.arrayOf(PropTypes.shape({
+    color: PropTypes.string.isRequired, // hex color
+    value: PropTypes.number.isRequired,
+  })).isRequired,
+};
---
+++ TimeTree/app.js
@@ -0,0 +1,172 @@
+/* eslint-disable react/no-multi-comp */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import createData from '../../tools/createData';
+import { Hierarchy } from '../../src';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import PieChart from './PieChart';
+
+const ENTER_EXIT_DURATION = 400;
+const UPDATE_DURATION = 700;
+
+var slices = [
+  { color: '#468966', value: 10 },
+  { color: '#FFF0A5', value: 20 },
+  { color: '#FFB03B', value: 30 },
+  { color: '#B64926', value: 40 },
+  { color: '#8E2800', value: 50 },
+];
+
+const initialValue = 0;
+
+const nodeLabelRenderer = (props) => {
+  return (
+    <g>
+      <circle r="4" />
+      <text x="5" y="0" transform="rotate(-45)">{props.data.data.name}</text>
+    </g>
+  );
+};
+
+const simpleRenderer = () => <circle r="4" />;
+
+const nodePieChartRenderer = () => {
+  return (
+    <g>
+      <PieChart size={20} slices={slices} />
+    </g>
+  );
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: String(initialValue),
+      data: createData(initialValue),
+      startDate: null,
+      endDate: null,
+      minimumChildren: 0,
+      hovered: null
+    };
+  }
+
+  changeMinimumChildren(value) {
+    this.setState({
+      minimumChildren: Number(value) || 0
+    });
+  }
+
+  changeStartDate(value) {
+    this.setState({
+      startDate: value
+    });
+  }
+
+  changeEndDate(value) {
+    this.setState({
+      endDate: value
+    });
+  }
+
+  changeValue(value) {
+    var intValue = parseInt(value);
+    if (!isNaN(intValue)) {
+      this.setState({
+        value,
+        data: createData(intValue)
+      });
+    } else {
+      this.setState({
+        value
+      });
+    }
+  }
+
+  render() {
+
+    const HoverRenderer = (BaseRenderer) => {
+      return (props) => {
+        return (
+          <g
+            onMouseEnter={() => this.setState({ hovered: props.data })}
+            onMouseLeave={() => this.setState({ hovered: null })}
+          >
+            <BaseRenderer {...props} />
+          </g>
+        );
+      };
+    };
+
+    return (
+      <div onDoubleClick={() => {
+        this.setState({
+          data: createData(parseInt(this.state.value)),
+          transition: {
+            enter: { wait: UPDATE_DURATION, duration: ENTER_EXIT_DURATION },
+            update: { wait: 0, duration: UPDATE_DURATION },
+            exit: { wait: 0, duration: 0 }
+          }
+        });
+      }}
+      >
+        <Hierarchy
+          onNodeClick={(node) => this.setState({
+            data: node,
+            transition: {
+              enter: { wait: UPDATE_DURATION + ENTER_EXIT_DURATION, duration: 0 },
+              update: { wait: ENTER_EXIT_DURATION, duration: UPDATE_DURATION },
+              exit: { wait: 0, duration: ENTER_EXIT_DURATION }
+            }
+          })}
+          nodeRenderer={HoverRenderer(nodePieChartRenderer)}
+          rootRenderer={HoverRenderer(simpleRenderer)}
+          leafRenderer={HoverRenderer(nodeLabelRenderer)}
+          startTime={this.state.startDate ? this.state.startDate.unix() * 1000 : null}
+          endTime={this.state.endDate ? this.state.endDate.unix() * 1000 : null}
+          data={this.state.data}
+          width={800}
+          height={880}
+          minimumChildren={this.state.minimumChildren}
+          transition={this.state.transition}
+        />
+        <br />
+        <input type="text" value={this.state.value} onChange={(e) => this.changeValue(e.target.value)} />
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.changeStartDate.bind(this)}
+        />
+        <DatePicker
+          selected={this.state.endDate}
+          onChange={this.changeEndDate.bind(this)}
+        />
+        <br />
+                Minimum children: <input type="text" value={this.state.minimumChildren} onChange={(e) => this.changeMinimumChildren(e.target.value)} />
+        <div>
+          <div>
+            {renderInfo(this.state.hovered)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function renderInfo(node) {
+  if (node) {
+    return (
+      <h2>
+                Hovered node: {node.data.name}
+      </h2>
+    );
+  }
+  return null;
+}
+
+ReactDOM.render(
+  <App />
+  ,
+  document.getElementById('example')
+);
